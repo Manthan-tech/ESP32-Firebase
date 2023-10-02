dbd55ff956bbdc286e24f86bdead4a283c3021d9
UniPickCTO <91817349+UniPickCTO@users.noreply.github.com>
2023-10-02 11:39:56 +0530

    Update OxyController.ino
    updates for 2023, delete in 2024

--- OxyController.ino
+++ OxyController.ino
@@ -13,6 +13,9 @@ void setup() {
   pinMode(solenoidA, OUTPUT);
   pinMode(solenoidB, OUTPUT);
   pinMode(potentiometer, INPUT);
+  pinMode(justfothacktober, INPUT);
+  pinMode(takeit, OUTPUT);
+  //delete above 2 lines
   ads.setGain(GAIN_SIXTEEN);
   Serial.begin(9600);
   ads.begin();
@@ -22,6 +25,7 @@ void loop() {
   Serial.print("User Value");
   requiredLevel=map(analogRead(potentiometer),0,1023,0,100);//read and map user value
   Serial.print(requiredLevel);
+  Serial.print("justprintit");
   int16_t adc0; // 16 bits ADC read of input A0
   adc0 = ads.readADC_SingleEnded(0);  //reading input Video Code
   Voltage = (float(adc0) * 0.1875 /15.28); //converting to voltage Video Code
@@ -37,6 +41,6 @@ else if(Oxy>float(requiredLevel+5)){
   Serial.print(adc0);
   Serial.print("\tVoltage: ");
   Serial.println(Voltage, 2);
-  Serial.println();  
+  Serial.println("Delete this line too");  
 delay(500);
 }
